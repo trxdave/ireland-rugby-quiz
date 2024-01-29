c421915276a35d7bf839bc88a6afb3cf04827408
David Cotter <daveguy28@gmail.com>
2024-01-29 13:16:02 +0000

    make out the question in Javascript

---
+++ assets/js/index.js
@@ -0,0 +1,88 @@
+let questions = [{
+    question: "Which player scored the most tries in the 2014 tournament?",
+    option1: "Andrew Trimble",
+    option2: "Johnny Sexton",
+    option3: "Rob Kearney",
+    answer: "2",
+},
+    {   
+    question: "Where did Ireland finish on the table in 2005?",
+    option1: "2nd",
+    option2: "3rd",
+    option3: "4th",
+    option4: "1st",
+    answer: "2",
+    },
+
+    {
+    question: "What was the final score of Ireland's Grand Slam winning match against Wales in 2009?",
+    option1: "14-16",
+    option2: "16-18",
+    option3: "15-17",
+    option4: "19-21",
+    answer: "3",
+    },
+
+    {
+    question: "Who did Ireland play in their very first Six Nations game back in 2000?",
+    option1: "Scotland",
+    option2: "Wales",
+    option3: "France",
+    option4: "England",
+    answer: "4",
+    },
+
+    {
+    question: "Who were Ireland playing in the 2007 final round before France denied them a championship on points difference?",
+    option1: "Italy",
+    option2: "Scotland",
+    option3: "Wales",
+    option4: "France",
+    answer: "1",
+    },
+
+    {
+    question: "Who scored Ireland's only try when they beat England in Twickenham in 2004?",
+    option1: "Brian O'Driscoll",
+    option2: "Girvan Dempsey",
+    option3: "Shane Horgan",
+    answer: "2",
+    },
+
+    {
+    question: "How many games did Ireland win in the 2008 tournament?",
+    option1: "Two",
+    option2: "Three",
+    option3: "Four",
+    option4: "One",
+    answer: "1"
+    },
+
+    {
+    question: "Who ended up playing on the wing against Italy in 2013?",
+    option1: "Sean Cronin",
+    option2: "Peter O'Mahony",
+    option3: "Sean O'Brien",
+    option4: "Paul O'Connell",
+    answer: "2",
+    },
+
+    {
+    question: "Who won Player of The Championship in 2010?",
+    option1: "Tommy Bowe",
+    option2: "Paul O'Connell",
+    option3: "Brian O'Driscoll",
+    option4: "Johnny Sexton",
+    answer: "1",
+    },
+
+    {
+    question: "In what was Ireland's game against France postponed due to frost?",
+    option1: "2010",
+    option2: "2011",
+    option3: "2012",
+    option4: "2009",
+    answer: "3",
+    },
+];
+
